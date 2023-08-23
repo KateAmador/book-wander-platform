a3e4cfee3156281c2fb161bc09f16dff4f42780e
KateAmador <kateamador13@gmail.com>
2023-08-23 14:34:05 -0500

    Implementado logout

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio | Book Wander</title>
 </head>
+
 <body>
     <h1>HOME</h1>
+
+    @auth
+    <p>Bienvenido {{auth()->user()->first_name }}</p>
+    <p>
+        <a href="/logout">Cerrar sesion</a>
+    </p>
+    @endauth
+
+    @guest
+    <p>Para ver el contenido <a href="/login">Iniciar sesion</a></p>
+    @endguest
 </body>
+
 </html>
