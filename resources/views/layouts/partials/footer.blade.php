ceb965887be4b275599222df1e0ce6cfffc53dcd
KateAmador <kateamador13@gmail.com>
2023-09-07 22:04:51 -0500

    Creado footer

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,61 @@
+<link href="{{ url('assets/css/footer.css') }}" rel="stylesheet">
+<footer>
+    <div class="container-footer-all">
+        <div class="container-body">
+
+            <div class="colunm1">
+                <h3>Book Wander</h3>
+                <p><a href="{{ route('home.about') }}">Quienes Somos</a></p>
+            </div>
+            <div class="column2">
+                <h3>Redes Sociales:</h3>
+                <div class="container-social">
+                    <div class="row">
+                        <div class="social-item">
+                            <img src="{{ url('assets/img/facebook.png') }}" alt="facebook">
+                            <a href="https://www.facebook.com/" target="_blank">Facebook</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="social-item">
+                            <img src="{{ url('assets/img/instagram.png') }}" alt="instagram">
+                            <a href="https://www.instagram.com/" target="_blank">Instagram</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="social-item">
+                            <img src="{{ url('assets/img/twitter.png') }}" alt="twitter">
+                            <a href="https://twitter.com/" target="_blank">Twitter</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="column3">
+                <h3>Contacto:</h3>
+                <div class="container-contact">
+                    <div class="row2">
+                        <label>contacto@bookwander.com</label>
+                    </div>
+                    <div class="row2">
+                        <label>+57 318 000 0000</label>
+                    </div>
+                    <div class="row2">
+                        <label><a href="">Formulario de Contacto</a></label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-footer">
+        <div class="footer">
+            <div class="copyright">
+                © 2023 Todos los derechos reservados | <a href="index.html">Book Wander</a>
+            </div>
+            <div class="informacion">
+                <a href="#">Politicas de Privacidad</a> | <a href="#">Terminos y Condiciones</a>
+            </div>
+        </div>
+    </div>
+</footer>
